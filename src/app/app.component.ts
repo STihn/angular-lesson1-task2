4d4039e729878f855694f9f2e444a903859cd2df
STihn <you.lucky@bk.ru>
2021-02-14 12:37:22 +0300

    lesson3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-lesson1';
+  param1 = 'angular hello';
+  inputValue ='';
+  inputValue2 ='';
 
   con(event: any) {
     console.log(event);
+  }
+
+  onInput(event) {
+    console.log(event)
+    this.inputValue = event.target.value;
+  }
+
+  onKeyDown(event: any) {
+    console.log(event);
+    this.inputValue2 = event.target.value;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Mod1Module } from './mod1/mod1.module';
+import { Mod2Module } from './mod2/mod2.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { Mod1Module } from './mod1/mod1.module';
   ],
   imports: [
     BrowserModule,
-    Mod1Module
+    Mod1Module,
+    Mod2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
